ef54d6eff942b28a68a649d35a4e637b5cc3df63
David Mora <dav.mora.santana@gmail.com>
2019-12-10 23:09:36 -0600

    Basic authentication for Delete Employee method.

--- src/main/java/com/api/config/SecurityConfig.java
+++ src/main/java/com/api/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.api.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,6 +10,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
+		http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/v1/employees/**").fullyAuthenticated().and().httpBasic();
 	}
 	
 }
